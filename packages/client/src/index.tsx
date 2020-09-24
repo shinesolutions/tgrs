7c68cde29465721ece71055e1047d02bf9f555d2
Ben Teese <ben.teese@shinesolutions.com>
2020-09-24 20:39:11 +1000

    Get client talking to server

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider
+      client={
+        new ApolloClient({
+          cache: new InMemoryCache(),
+          link: ApolloLink.from([
+            // Add an authorization header to each outgoing request
+            setContext(() => ({
+              headers: {
+                Authorization:
+                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+              },
+            })),
+            new HttpLink({ uri: "http://localhost:4000" }),
+          ]),
+        })
+      }
+    >
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
