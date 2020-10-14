3e652ecb43664c27099a18f7f2b6443b679d94cb
Ben Teese <ben.teese@shinesolutions.com>
2020-10-14 20:44:31 +1100

    Improve & enable client unit test

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { render, waitFor, screen } from "@testing-library/react";
+import App from "./App";
+import { setupServer } from "msw/node";
+import { graphql } from "msw";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Setup requests interception using the given handlers.
+const server = setupServer();
+
+beforeAll(() => {
+  // Enable the mocking in tests.
+  server.listen();
+});
+
+afterEach(() => {
+  // Reset any runtime handlers tests may use.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Clean up once the tests are done.
+  server.close();
+});
+
+describe("App", () => {
+  it("renders learn react link", async () => {
+    server.use(
+      graphql.query("AppQuery", (_, res, ctx) =>
+        res(
+          ctx.data({
+            personalizedGreeting: "Hello, Unit Test!",
+          })
+        )
+      )
+    );
+
+    render(
+      <ApolloProvider
+        client={
+          new ApolloClient({
+            cache: new InMemoryCache(),
+            link: new HttpLink(),
+          })
+        }
+      >
+        <App />
+      </ApolloProvider>
+    );
+    const heading = await waitFor(() => screen.getByRole("heading"));
+    expect(heading).toHaveTextContent("Hello, Unit Test!");
+  });
 });
