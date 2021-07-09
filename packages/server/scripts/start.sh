325d7a64d1f490c878792ded29b6590a28eb4277
Sergio Martinez <sergio.martinez@shinesolutions.com>
2021-07-09 11:39:45 +1000

    fix(server-start): error running concurrently. In order to use the node_module dependency, yarn run needs to be removed.

--- packages/server/scripts/start.sh
+++ packages/server/scripts/start.sh
@@ -1,2 +1,2 @@
-yarn run concurrently "yarn codegen --watch" \
-    "yarn run nodemon --ext ts,graphql,json --exec 'ts-node' scripts/startDevServer"
+concurrently "codegen --watch" \
+    "nodemon --ext ts,graphql,json --exec 'ts-node' scripts/startDevServer"
