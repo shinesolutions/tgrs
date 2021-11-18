3828432cd948dda36a9519f59b592649df38f445
Ben Teese <ben.teese@shinesolutions.com>
2021-11-18 21:03:24 +1100

    Remove duplicated strict mode

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { gql, useQuery } from "@apollo/client";
 import { AppQuery, AppQueryVariables } from "./__generated__/AppQuery";
 import { Language } from "./__generated__/globalTypes";
-import React from "react";
 
 function App() {
   const { data, loading, error } = useQuery<AppQuery, AppQueryVariables>(
@@ -19,17 +18,15 @@ function App() {
   );
 
   return (
-    <React.StrictMode>
-      <div className="App">
-        {loading ? (
-          <p role="alert">Loading...</p>
-        ) : error ? (
-          <p role="alert">Error!</p>
-        ) : (
-          <h1>{data?.personalizedGreeting}</h1>
-        )}
-      </div>
-    </React.StrictMode>
+    <div className="App">
+      {loading ? (
+        <p role="alert">Loading...</p>
+      ) : error ? (
+        <p role="alert">Error!</p>
+      ) : (
+        <h1>{data?.personalizedGreeting}</h1>
+      )}
+    </div>
   );
 }
 
